d2b6f9d0d3ac2e127cade9f1c19ca011863d0cb2
Kian Becera <154484943+thy-kian@users.noreply.github.com>
2024-08-10 17:51:35 +0800

    Adding Files

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+
+  <title>Portfolio</title>
+
+</head>
+
+<body>
+<!-- Header -->
+    <header class="header">
+      
+    <!-- Navigation -->
+      <!-- <img src="../img/icons/letterk.png" alt="" class="img1"> -->
+      <img src="../../../img/icons/letterk.png" class="img1">
+      <a href="#" class="logo">
+        Kian.
+      </a>
+
+      <nav class="navbar">
+        <a href="" class="active">Home</a>
+        <a href="">About</a>
+        <a href="">Service</a>
+        <a href="">Portfolio</a>  
+        <a href="">Contact</a>
+      </nav>
+
+      <section class="home">
+        
+      </section>
+    </header>  
+
+</body>
+</html>
